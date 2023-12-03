8b164c778f579c9c81f6b77c2f0c9f7eb4784117
Royson Lobo <royrenlobo@gmail.com>
2023-12-04 00:30:05 +0530

    changes in webhook.js again

--- src/functions/webhooks.js
+++ src/functions/webhooks.js
@@ -1,5 +1,4 @@
-// functions/webhook.js
-const fetch = import('node-fetch');
+const fetch = require('node-fetch');
 
 const api = {
   key: '0c5429a0280f3841c867a3f9e8e3b708',
@@ -7,13 +6,23 @@ const api = {
 };
 
 exports.handler = async function (event, context) {
-  const { queryResult } = JSON.parse(event.body);
-  const location = queryResult.parameters['geo-city'];
-
   try {
+    // Check if event.body is not empty
+    if (!event.body) {
+      throw new Error('Request body is empty.');
+    }
+
+    const { queryResult } = JSON.parse(event.body);
+    const location = queryResult.parameters['geo-city'];
+
     const response = await fetch(
       `${api.base}weather?q=${location}&units=metric&APPID=${api.key}`
     );
+
+    if (!response.ok) {
+      throw new Error(`Weather API request failed with status ${response.status}`);
+    }
+
     const weatherData = await response.json();
     const responseText = `The weather in ${location} is ${weatherData.main.temp} degrees Celsius with ${weatherData.weather[0].description}.`;
 
@@ -22,11 +31,11 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ fulfillmentText: responseText }),
     };
   } catch (error) {
-    console.error('Error fetching weather data:', error);
+    console.error('Error in the function:', error);
+
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Internal Server Error' }),
     };
   }
 };
-
